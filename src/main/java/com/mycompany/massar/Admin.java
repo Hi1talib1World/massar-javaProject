2d2e3aa072ddad1f04e72d04e339b78620abd66d
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-01 01:58:52 +0100

    Create Admin.java

---
+++ src/main/java/com/mycompany/massar/Admin.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.massar;
+
+
+/**
+ *
+ * @author mahajan
+ */
+public class Admin {
+    
+
+    public static Admin masterClass;
+    private static double treasury = 0.0;
+    
+    private Admin()
+    {
+       
+    }
+    
+    public static Admin getInstance() {
+        if (masterClass == null) {
+            masterClass = new Admin();
+        }
+        return masterClass;
+    }
+
+    /**
+     *
+     * @return
+     */
+    
+    
+    public static void pay(Double amount) {
+        treasury += amount;
+    }
+    
+    public static double getTreasury() {
+        return treasury;
+    }
+    
+    
+    
+}
