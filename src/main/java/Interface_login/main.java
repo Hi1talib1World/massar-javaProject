1d39a1b257d74dcc54367346365354c687d75405
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-08 03:30:26 +0100

    Update main.java

--- src/main/java/Interface_login/main.java
+++ src/main/java/Interface_login/main.java
@@ -33,7 +33,7 @@ public class main extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 469, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
