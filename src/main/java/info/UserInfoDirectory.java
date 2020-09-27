b8b692fefc199528e005469e7d9ea908724a2682
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-09-27 12:21:06 +0100

    Update UserInfoDirectory.java

--- src/main/java/info/UserInfoDirectory.java
+++ src/main/java/info/UserInfoDirectory.java
@@ -5,10 +5,41 @@
  */
 package info;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class UserInfoDirectory {
+    private ArrayList<UserInfo> userInfoDirectory;
+
+    public UserInfoDirectory() {
+        this.userInfoDirectory = new ArrayList<UserInfo>();
+    }
     
+    public ArrayList<UserInfo> getUserAccountDirectory() {
+        return userInfoDirectory;
+    }
+
+    public void setUserAccountDirectory(ArrayList<UserInfo> userAccountDirectory) {
+        this.userInfoDirectory = userAccountDirectory;
+    }
+    
+    public void addUserAccount(UserInfo ua) {
+        userInfoDirectory.add(ua);
+    }
+    
+    public void removeUserAccount(UserInfo userAccount){
+        userInfoDirectory.remove(userAccount);
+    }
+    
+    public UserInfo getUserAccount(String userName) {
+        for (UserInfo ua: userInfoDirectory) {
+            if (ua.getUsername().equals(userName)) {
+                return ua;
+            }
+        }
+        return null;
+    }
 }
