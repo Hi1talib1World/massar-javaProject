a0ee770e33e42be4c7521b54199c287c6207c54b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-09-27 12:35:47 +0100

    Update Configre.java

--- src/main/java/info/Configre.java
+++ src/main/java/info/Configre.java
@@ -19,23 +19,23 @@ public class Configre {
         Person p = new Person();
         p.setFirstName("Admin");
         p.setLastName("Admin");
-        UserInfo ua = new UserInfo("admin", "admin", UserAccount.ADMIN_ROLE, p);
-        msc.getUserAccountDirectory().addUserAccount(ua);
+        UserInfo ua = new UserInfo("admin", "admin", UserInfo.ADMIN_ROLE, p);
+        msc.getUserInfoDirectory().addUserAccount(ua);
         //Adding Retailer 1
         Retailer r = new Retailer();
         r.setRetailerName("Apple");
         r.setZipCode("02115");
         msc.getRetailerCatalogue().addRetailer(r);
-        UserInfo uar = new UserInfo("apple", "apple", UserAccount.RETAILER_ROLE, r);
-        msc.getUserAccountDirectory().addUserAccount(uar);    
+        UserInfo uar = new UserInfo("apple", "apple", UserInfo.RETAILER_ROLE, r);
+        msc.getUserInfoDirectory().addUserAccount(uar);    
         
         //Adding Retailer 2
         Retailer r1 = new Retailer();
         r1.setRetailerName("Tesla");
         r1.setZipCode("02115");
         msc.getRetailerCatalogue().addRetailer(r1);
-        UserInfo uar1 = new UserInfo("tesla", "tesla", UserAccount.RETAILER_ROLE, r1);
-        msc.getUserAccountDirectory().addUserAccount(uar1);
+        UserInfo uar1 = new UserInfo("tesla", "tesla", UserInfo.RETAILER_ROLE, r1);
+        msc.getUserInfoDirectory().addUserInfo(uar1);
         
         
         //Adding Retailer 3
@@ -44,13 +44,13 @@ public class Configre {
         r3.setZipCode("02116");
         msc.getRetailerCatalogue().addRetailer(r3);
         UserInfo uar2 = new UserInfo("macys", "macys", UserInfo.RETAILER_ROLE, r3);
-        msc.getUserAccountDirectory().addUserAccount(uar2);
+        msc.getUserInfoDirectory().addUserInfo(uar2);
         
         
         Customer per = new Customer("08-21-1995", "Tom","02115");
         msc.getCustomerCatalogue().addCustomer(per);
         UserInfo uac = new UserInfo("cust", "cust", UserInfo.CUSTOMER_ROLE, per);
-        msc.getUserAccountDirectory().addUserAccount(uac);
+        msc.getUserInfoDirectory().addUserInfo(uac);
         
         
         return msc;
